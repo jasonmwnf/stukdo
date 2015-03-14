af80c905551db3dcb3099de08cf901d535230eda
Jason Williams <jasonmw_nf@hotmail.com>
2015-03-14 12:19:55 -0230

    added task functionality

--- stukdo/db/schema.rb
+++ stukdo/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312191327) do
+ActiveRecord::Schema.define(version: 20150314144348) do
+
+  create_table "tasks", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
